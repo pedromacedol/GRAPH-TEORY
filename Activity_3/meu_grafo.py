071c78c5708c9b445a77c5995a0b78e8bcce5649
Pedro Macedo <36522521+pedromacedol@users.noreply.github.com>
2021-08-13 00:17:22 -0300

    Improve fuctions dfs, bfs and conexo

--- Activity_3/meu_grafo.py
+++ Activity_3/meu_grafo.py
@@ -1,11 +1,12 @@
 from bibgrafo.aresta import Aresta
 from bibgrafo.grafo_lista_adjacencia import GrafoListaAdjacencia
 from bibgrafo.grafo_exceptions import *
+import copy
 
 class MeuGrafo(GrafoListaAdjacencia):
     def __init__(self, x):
         super().__init__(x)
-        self.dfs_grafo = GrafoListaAdjacencia(self.N)
+        self.dfs_grafo = GrafoListaAdjacencia()
         self.dfs_visitados = []
 
         self.bfs_grafo = GrafoListaAdjacencia(self.N)
@@ -139,49 +140,57 @@ class MeuGrafo(GrafoListaAdjacencia):
         Realiza a busca pela árvore DFS do grafo a partir do vértice indicado.
         :parametro V: O vértice a ser analisado
         :return: Uma árvore DFS, representada por meio de um outro grafo que contém apenas as arestas que fazem parte da árvore.
-        :raises: raise Exception se o vértice não existe ou existe laço no grafo 
+        :raises: raise VerticeInvalidoException
         '''
-
-        if self.ha_laco() == False and self.verticeValido(V) == True:
-            chave = False
-
-            if len(self.N) == len(self.dfs_visitados):
-                    self.c = 0
-                    self.dfs_visitados = []
-                    dfs_grafo = self.dfs_grafo
-                    self.dfs_grafo = GrafoListaAdjacencia(self.N)
-                    return dfs_grafo
-            
+        if self.verticeValido(V) == True:
+            if self.dfs_visitados == []:
+                self.grafo = copy.deepcopy(self)
+                self.grafo.caminho_remover_paralelas()
+        
             if V not in self.dfs_visitados:
-
                 self.dfs_visitados.append(V)
-
-            # PECORRENDO AS ARESTAS SOBRE O VERTICE 
-            for a in self.A:
-                    
-                V1 = self.A[a].getV1()
-                V2 = self.A[a].getV2()
-                    
-                if V1 == V and V2 not in self.dfs_visitados:
-                    
-                    self.dfs_grafo.adicionaAresta(a, V1, V2)
-                    chave = True
-                    self.c += 1
-                    return self.dfs(self.A[a].getV2())
-
-                elif V2 == V and V1 not in self.dfs_visitados:
-                    
-                    self.dfs_grafo.adicionaAresta(a, V1, V2)
-                    self.c += 1
-                    chave = True
-                    return self.dfs(self.A[a].getV1())        
-
-            if chave == False:
-                self.c -= 1
-                return self.dfs(self.dfs_visitados[self.c])
+                if V not in self.dfs_grafo.N:
+                    self.dfs_grafo.adicionaVertice(V)
             
+            if self.grafo.A != []:
+                self.dfs_recursiva(V)
+
+            return self.dfs_grafo
         else:
-            raise Exception('Grafo inválido, digite um grafo conectado e/ou sem laço(s).')
+            raise VerticeInvalidoException
+            
+    def dfs_recursiva(self, v):
+        '''
+        Verifica os vertices adjacentes a v e retorna o vertice para prosseguir
+        :return: funcao dfs com um vertice como parâmetro
+        '''
+        
+        if self.grafo.arestas_sobre_vertice(v) == []:
+            for n in reversed(self.dfs_grafo.N):
+                if self.grafo.arestas_sobre_vertice(n) != []:
+                    v = n
+        
+        for a in self.grafo.arestas_sobre_vertice(v):
+            
+            vertice_1 = self.grafo.A[a].getV1()
+            vertice_2 = self.grafo.A[a].getV2()
+    
+            if vertice_1 == v and vertice_2 not in self.dfs_grafo.N:
+               
+                self.dfs_grafo.adicionaVertice(vertice_2)
+                self.dfs_grafo.adicionaAresta(a, vertice_1, vertice_2)
+                self.grafo.removeAresta(a)
+                return self.dfs(vertice_2)
+            
+            elif vertice_2 == v and vertice_1 not in self.dfs_grafo.N:
+                self.dfs_grafo.adicionaVertice(vertice_1)
+                self.dfs_grafo.adicionaAresta(a,vertice_1, vertice_2)
+                self.grafo.removeAresta(a)
+                return self.dfs(vertice_1)
+
+            else:
+                self.grafo.removeAresta(a)
+                return self.dfs_recursiva(v)
     
     def bfs(self, V=''):
 
@@ -191,11 +200,9 @@ class MeuGrafo(GrafoListaAdjacencia):
         :return: Uma árvore BFS, representada por meio de um outro grafo que contém apenas as arestas que fazem parte da árvore.
         :raises: raise Exception se o vértice não existe ou existe laço no grafo.
         '''
-
-        if self.ha_laco() == False and self.verticeValido(V):
-
+        
+        if self.conexo() == True and self.verticeValido(V):
             chave = False
-
             self.bfs_visitados.append(V)
 
             for ar in self.arestas_sobre_vertice(V):
@@ -204,7 +211,6 @@ class MeuGrafo(GrafoListaAdjacencia):
                 V2 = self.A[ar].getV2()
 
                 if V1 == V and V2 not in self.bfs_visitados and V2 not in self.bfs_vertices:
-                    
                     self.bfs_grafo.adicionaAresta(ar, V1, V2)
                     self.bfs_vertices.append(V2)
                     chave = True
@@ -225,17 +231,21 @@ class MeuGrafo(GrafoListaAdjacencia):
 
             if chave == False:
                 self.bfs_vertices.pop(0)
-                return(self.bfs(self.bfs_vertices[0]))
-
+                if self.bfs_vertices != []:
+                    return(self.bfs(self.bfs_vertices[0]))
+                else:
+                    bfs_grafo = self.bfs_grafo 
+                    self.bfs_grafo = GrafoListaAdjacencia(self.N)
+                    return bfs_grafo 
+                    
             if V in self.bfs_vertices and chave == True:
                 self.bfs_vertices.pop(0)
                 return(self.bfs(self.bfs_vertices[0]))
-
             if V not in self.bfs_vertices and chave == True:
                 return(self.bfs(self.bfs_vertices[0]))
 
         else:
-            raise Exception('Grafo inválido, digite um grafo conectado e/ou sem laço(s).')
+            raise Exception('Grafo inválido, digite um grafo conexo.')
     
     def ha_ciclo(self):
         '''
@@ -354,15 +364,11 @@ class MeuGrafo(GrafoListaAdjacencia):
         Verifica se o grafo é ou não conexo
         :return: Um valor booleano que indica se o grafo é conexo.
         '''
-        grau_menor = 0
-        for N in self.N:
-            if N == list(self.N)[0] or self.grau(N) < grau_menor:
-                grau_menor = self.grau(N)
-        if grau_menor < 1:
-            return False
-        else:
+        if len(self.dfs(self.N[0]).N) == len(self.N):
             return True
-
+        else:
+            return False
+    
     def caminho(self, n):
         '''
         Pecorre o grafo e retonar um caminho de tamanho 'n' em lista no formato [v1, a1, v2, a2, v3, a3, ...] ou
